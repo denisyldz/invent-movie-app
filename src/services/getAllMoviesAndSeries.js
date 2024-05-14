80003414a610d0b21076a80cfdd18c96108fe940
Deniz YILDIZ <100310188+denisyldz@users.noreply.github.com>
2024-05-15 00:39:42 +0300

    indentation

--- src/services/getAllMoviesAndSeries.js
+++ src/services/getAllMoviesAndSeries.js
@@ -1,43 +1,43 @@
 import { apiKey } from '../constants/key';
 
 export const getMoviesAndSeries = async (searchText, searchType) => {
-    let allData = [];
-    let totalPages = 1;
+  let allData = [];
+  let totalPages = 1;
 
-    try {
-        for (let page = 1; page <= totalPages; page++) {
-            const url = `http://www.omdbapi.com/?s=${searchText}&type=${searchType}&page=${page}&apikey=${apiKey}`;
-            const response = await fetch(url);
+  try {
+    for (let page = 1; page <= totalPages; page++) {
+      const url = `http://www.omdbapi.com/?s=${searchText}&type=${searchType}&page=${page}&apikey=${apiKey}`;
+      const response = await fetch(url);
 
-            if (!response.ok) {
-                throw new Error('Failed to fetch data');
-            }
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
 
-            const responseJson = await response.json();
-            allData = allData.concat(responseJson.Search);
+      const responseJson = await response.json();
+      allData = allData.concat(responseJson.Search);
 
-            totalPages = Math.ceil(responseJson.totalResults / 10);
-        }
-    } catch (error) {
-        console.error(error);
+      totalPages = Math.ceil(responseJson.totalResults / 10);
     }
+  } catch (error) {
+    console.error(error);
+  }
 
-    return allData;
+  return allData;
 }
 
 export const getByTitle = async (title) => {
-    try {
-        const url = `http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`;
-        const response = await fetch(url);
+  try {
+    const url = `http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`;
+    const response = await fetch(url);
 
-        if (!response.ok) {
-            throw new Error('Failed to fetch data');
-        }
-
-        const responseJson = await response.json();
-        return responseJson;
-    } catch (error) {
-        console.error(error);
-        return null;
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
     }
+
+    const responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
